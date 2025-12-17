fc6996b105ebecac00e54731e0615c287217e938
Ranjeet Raushan <rkraushansdm@gmail.com>
2025-12-17 22:04:50 +0530

    Initial Commit

---
+++ AcharyaPrashantDemoTask/AcharyaPrashantDemoTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcharyaPrashantDemoTaskApp.swift
+//  AcharyaPrashantDemoTask
+//
+//  Created by Ranjeet  Raushan on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AcharyaPrashantDemoTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
